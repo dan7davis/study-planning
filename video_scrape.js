7b56c8df5619271cce17f6027e295f118db37901
caspar <casparkrijgsman@gmail.com>
2017-08-29 17:34:08 +0200

    bug fix

--- video_scrape.js
+++ video_scrape.js
@@ -2,14 +2,16 @@
 url = "https://insights.edx.org/courses/course-v1:DelftX+EnergyX+3T2017/engagement/videos/";
 
 
-// Begin Snippet B
+// Begin Snippet A
 var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 setTimeout(scrape(), 3000);
+// End Snippet A
 
 
+// Begin Snippet B
 function scrape(){
     $('tbody').children().removeClass().addClass("video_row");
 
@@ -28,4 +30,6 @@ function scrape(){
     copy(vidMap);
 }
 
-// End Snbippet B
+scrape();
+
+// End Snippet B
